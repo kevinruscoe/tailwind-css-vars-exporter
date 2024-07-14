fd643f1b7e5ae47f866427cd3a95d1aba223a786
Kevin Ruscoe <kevinr@cleardesign.co.uk>
2024-07-14 14:30:56 +0100

    process the DEFAULT keyword

--- index.js
+++ index.js
@@ -12,24 +12,31 @@ module.exports = function (rules) {
       const themeData = theme(rule);
 
       const buildOutput = (obj, prefix) => {
-        if (typeof obj === "object" && !Array.isArray(obj)) {
-          return Object.keys(obj).reduce((acc, key) => {
-            const newPrefix = [prefix, key].join("-");
-            if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
-              Object.assign(acc, buildOutput(obj[key], newPrefix));
-            } else if (Array.isArray(obj[key])) {
-              acc[newPrefix] = obj[key].join(", ");
-            } else {
-              acc[newPrefix] = obj[key];
+        if (typeof obj === 'object' && !Array.isArray(obj)) {
+            let result = Object.keys(obj).reduce((acc, key) => {
+                if (key === 'DEFAULT') return acc;
+                const newPrefix = `${prefix}-${key}`;
+                if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+                    Object.assign(acc, buildOutput(obj[key], newPrefix));
+                } else if (Array.isArray(obj[key])) {
+                    acc[newPrefix] = obj[key].join(', ');
+                } else {
+                    acc[newPrefix] = obj[key];
+                }
+                return acc;
+            }, {});
+
+            if (obj.DEFAULT !== undefined) {
+                result[prefix] = obj.DEFAULT;
             }
-            return acc;
-          }, {});
+
+            return result;
         } else if (Array.isArray(obj)) {
-          return { [prefix]: obj.join(", ") };
+            return { [prefix]: obj.join(', ') };
         } else {
-          return { [prefix]: obj };
+            return { [prefix]: obj };
         }
-      };
+    };
 
       return buildOutput(themeData, `--${rule.split(".").join("-")}`);
     };
